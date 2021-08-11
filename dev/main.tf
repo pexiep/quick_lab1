bee53ff48e1e1376d3846c9d7e0a00e727391f71
pexiep <74528809+pexiep@users.noreply.github.com>
2021-08-11 18:40:31 +0700

    Add files via upload

--- dev/main.tf
+++ dev/main.tf
@@ -18,10 +18,11 @@ provider "aws" {
 module "my_vpc" {
     source = "../Modules/VPC"
 
+    vpc_name             = "non-prod"
     vpc_cidr             = "10.1.0.0/16"
-    tenancy              = "default"
-    vpc_public_subnets   = ""
-    vpc_id               = "${modules.vpc.vpc_id}"
-   
+    vpc_azs              = ["us-east-1a", "us-east-1b", "us-east-1c"]
+    vpc_private_subnets  = ["10.1.10.0/24", "10.1.11.0/24", "10.1.22.0/24"]
+    vpc_public_subnets   = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
+    vpc_db_subnets       = ["10.1.100.0/24", "10.1.110.0/24", "10.1.220.0/24"]
 }
 
