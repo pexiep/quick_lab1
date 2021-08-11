9f81ef0f333bbbddc867fda0bfc896c9adb804b7
pexiep <74528809+pexiep@users.noreply.github.com>
2021-08-11 21:10:12 +0700

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -2,22 +2,6 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 #1 - Create VPC
 
 resource "aws_vpc" "nonprod" {
